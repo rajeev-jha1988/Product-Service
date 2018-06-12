0ec177f39b3827d8e7aeed31d61f2686234b49ec
rajeev.jha <raj.k9732@gmail.com>
2018-06-12 23:36:43 +0530

    test message
    Change-Id: Ic8e0c0e826a4e3fbc9059b824bf94795a70e14a9
    Signed-off-by: rajeev.jha <raj.k9732@gmail.com>

---
+++ src/main/java/com/raj/mybazzar/app/controller/MessageRestController.java
@@ -0,0 +1,20 @@
+package com.raj.mybazzar.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @GetMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
